f87a905de722a9eff5b00cfb2b12ab118e9cb299
njmayes <amayesingnathan123@gmail.com>
2026-04-14 21:19:24 +0100

    Add deployment specific links

--- nmayes.dev/nmayes.dev.Client/Program.cs
+++ nmayes.dev/nmayes.dev.Client/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using nmayes.dev.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.Configure<ProjectLinksOptions>(
+    builder.Configuration.GetSection("ProjectLinks"));
 
 await builder.Build().RunAsync();
---
+++ nmayes.dev/nmayes.dev.Client/Settings.cs
@@ -0,0 +1,6 @@
+﻿namespace nmayes.dev.Client;
+
+public class ProjectLinksOptions
+{
+    public string SteelPans { get; set; } = string.Empty;
+}
